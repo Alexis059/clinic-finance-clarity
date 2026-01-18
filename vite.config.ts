d5bfa45a02f1d4c824f6c8306e16dfb6b5be001b
Aleksey <35919299+Alexis059@users.noreply.github.com>
2026-01-18 17:52:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- base: mode === "production" ? "/clinic-finance-clarity/" : "/",
+ base: "/clinic-finance-clarity/",
   server: {
     host: "::",
     port: 8080,
